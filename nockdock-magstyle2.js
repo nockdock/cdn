173c94c30831b3a726c75eae310875e753681867
nockdock <46267165+nockdock@users.noreply.github.com>
2018-12-30 19:04:36 -0300

    Update nockdock-magstyle2.js

--- nockdock-magstyle2.js
+++ nockdock-magstyle2.js
@@ -1,4 +1,17 @@
 //<![CDATA[
+//Pager
+$(document).ready(function(){
+var olderLink = $(&quot;a.blog-pager-older-link&quot;).attr(&quot;href&quot;);
+$(&quot;a.blog-pager-older-link&quot;).load(olderLink+&quot; .post-title:first&quot;, function() {
+var olderLinkTitle = $(&quot;a.blog-pager-older-link&quot;).text();
+$(&quot;a.blog-pager-older-link&quot;).text(olderLinkTitle);//rgt
+});
+var newerLink = $(&quot;a.blog-pager-newer-link&quot;).attr(&quot;href&quot;);
+$(&quot;a.blog-pager-newer-link&quot;).load(newerLink+&quot; .post-title:first&quot;, function() {
+var newerLinkTitle = $(&quot;a.blog-pager-newer-link:first&quot;).text();
+$(&quot;a.blog-pager-newer-link&quot;).text(newerLinkTitle);
+});
+});
 //Pre Auto Selection
 $(&#39;i[rel=&quot;pre&quot;]&#39;).replaceWith(function() {
     return $(&#39;<pre><code>&#39; + $(this).html() + &#39;</code></pre>&#39;);
